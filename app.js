000f3aed756930af7447d789b3d923d964df2a34
Ousmanbah10 <ousmanbah3903244@gmail.com>
2025-01-31 05:03:35 -0500

    Connected Database

---
+++ app.js
@@ -0,0 +1,28 @@
+const connectDB = require("./db/connect");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = 3500;
+const task = require("./routes/tasks");
+
+// get middeware data
+app.use(express.json());
+
+//  routess
+app.get("/", (req, res, next) => {
+  res.send(`HEY OUSMAN`);
+});
+
+app.use("/api/v1/tasks", task);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+    app.listen(port, console.log(`Server Started Successfully on ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
