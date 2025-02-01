6abab9881dfbeb8ac2bc62f107c5b0955ba809cb
Ousmanbah10 <ousmanbah3903244@gmail.com>
2025-02-01 01:51:18 -0500

    Add Error Message for Unknown url

--- app.js
+++ app.js
@@ -4,18 +4,14 @@ const app = express();
 require("dotenv").config();
 const port = 3500;
 const task = require("./routes/tasks");
-
+const notfound = require("./middleware/notfound");
 // get middeware data
 app.use(express.static("./public"));
 app.use(express.json());
 
-//  routess
-app.get("/", (req, res, next) => {
-  res.send(`HEY OUSMAN`);
-});
-
+// routes
 app.use("/api/v1/tasks", task);
-
+app.use(notfound);
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
